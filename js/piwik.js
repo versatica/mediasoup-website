77589b1d8e60a7e387a8bdc6d575a9a6d097e7e8
Iñaki Baz Castillo <ibc@aliax.net>
2016-04-26 20:59:27 +0200

    Add piwik stuff

---
+++ js/piwik.js
@@ -0,0 +1,16 @@
+var _paq = _paq || [];
+
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
+(function()
+{
+	var u=(('https:' == document.location.protocol) ? 'https' : 'http') + '://private.versatica.com/piwik/';
+
+	_paq.push(['setTrackerUrl', u+'piwik.php']);
+	_paq.push(['setSiteId', 8]);
+
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+
+	g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
